03175409e05397a3494dad9ed7864c4632feae89
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2025-05-18 04:22:31 +0300

    fix: client will always retry on error

---
+++ CloudSync/CloudSync.GoogleDrive/Extensions/GoogleApiExceptionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Google;
+
+namespace CloudSync.GoogleDrive.Extensions;
+
+public static class GoogleApiExceptionExtensions
+{
+    public static bool ContainsReason(this GoogleApiException ex, string reason)
+    {
+        return ex.Error.Errors.Any(e => e.Reason == reason);
+    }
+}
