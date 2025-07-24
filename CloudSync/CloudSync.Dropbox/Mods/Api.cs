053be27ef96ac83025a868d03773f440f79edf1b
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2025-07-25 01:43:32 +0300

    refactor api code

---
+++ CloudSync/CloudSync.Dropbox/Mods/Api.cs
@@ -0,0 +1,9 @@
+﻿using StardewUI.Framework;
+
+namespace CloudSync.Dropbox.Mods;
+
+public static class Api
+{
+    public static IViewEngine? ViewEngine = null;
+    public static string ViewsPrefix = "Mods/FawazT.CloudSync.Dropbox/Views";
+}
